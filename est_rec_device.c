10f584495ad9036892dec5d1085a52d7ff775b0b
bob <daniel@carbonallstars.org>
2018-07-20 20:33:57 +0200

    Backend\n\nRecording channel estimator. Moved logic into method.\nWill be integrated into Backend.

--- est_rec_device.c
+++ est_rec_device.c
@@ -32,25 +32,19 @@ return the number of successful channels...
 
 
 
-static int _record_callback();
-
 typedef struct {
     unsigned int num_channels;
-    unsigned int itcount;
     bool chlimit_reached;
-} testdata_t;
+} estdata_t;
 
-int main(int argc, char** argv) {
-    (void) argc;
-    (void) argv;
+static int _est_record_callback();
 
-
-
-    PaError err = Pa_Initialize();
-    if (err != paNoError)
-        goto error_pa;
+void est_num_input_channels(PaDeviceIndex device, int* result_num_channel, int* error)
+{
+    *error = 0;
+    *result_num_channel = 0;
     PaStreamParameters strParams;
-    strParams.device = Pa_GetDefaultInputDevice();
+    strParams.device = device;
     if (strParams.device==paNoDevice)
         goto error_pa; 
     // NOTE paNonInterleaved 
@@ -59,78 +53,90 @@ int main(int argc, char** argv) {
     strParams.suggestedLatency = Pa_GetDeviceInfo(strParams.device)->defaultLowInputLatency;
     strParams.hostApiSpecificStreamInfo = NULL;
 
-    testdata_t* data = malloc(sizeof(testdata_t));
+    
+    estdata_t* data = malloc(sizeof(estdata_t));
     data->num_channels = 1;
     data->chlimit_reached = false;
 
+    PaError err = paNoError;
 
     while(true) {
 
-        fprintf(stderr, "Testing with %u channels \n", data->num_channels);
+        fprintf(stderr, "Testing with %u channels \n", 
+                            data->num_channels);
 
         strParams.channelCount = data->num_channels;
-        data->itcount = 0;
 
         PaStream* stream; 
-        err = Pa_OpenStream(
-                            &stream, &strParams, NULL,
+        err = Pa_OpenStream(&stream, &strParams, NULL,
                             44100, paFramesPerBufferUnspecified,
                             paDitherOff | paClipOff, 
-                            _record_callback,
+                            _est_record_callback,
                             data); 
-        if (err!=paNoError)
-            goto error_openstream;
+        if (err!=paNoError) {
+            *error = err;
+            break;
+        }
 
         err = Pa_StartStream(stream);
-        if (err!=paNoError)
-            goto error_startstream;
+        if (err!=paNoError) {
+            *error = err;
+            break;
+        }
 
         Pa_Sleep(10);
 
         Pa_StopStream(stream);
         Pa_CloseStream(stream);
 
-        fprintf(stderr, "Test %s\n", (data->chlimit_reached?"Not Ok":"Ok"));
+        fprintf(stderr, "Test %s\n", 
+                            (data->chlimit_reached?"Not Ok":"Ok"));
 
-
-        if (data->chlimit_reached)
-            goto error_errordata;
+        if (data->chlimit_reached) {
+            *error = err;
+            *result_num_channel = data->num_channels-1;
+            break;
+        }
 
         data->num_channels ++;
     }
+    free(data);
+
+error_pa:
+    ;
+}
 
 
-    goto return_ok;
+int main(int argc, char** argv) {
+    (void) argc;
+    (void) argv;
+
+    PaError err = Pa_Initialize();
+    if (err != paNoError)
+        goto error_pa;
+
+    int error;
+    int num_channel;
+    est_num_input_channels(Pa_GetDefaultInputDevice(), &num_channel, &error);
+
+    if (error!=paNoError)
+        goto error_pa;
+    else
+        goto result_ok;
 
 
     error_pa:
         Pa_Terminate();
         fprintf(stderr, "Portaudio error %s\n", Pa_GetErrorText(err));
-        goto return_err;
-    error_openstream:
-        Pa_Terminate();
-        fprintf(stderr, "Could not open stream with %u channels %s\n", data->num_channels, Pa_GetErrorText(err));
-        goto return_err;
-    error_startstream:
-        Pa_Terminate();
-        fprintf(stderr, "Could start stream with %u channels %s\n", data->num_channels, Pa_GetErrorText(err));
-        goto return_err;
-    error_errordata:
-        Pa_Terminate();
-        fprintf(stderr, "The device seems to have %u input channels %s\n", data->num_channels-1, Pa_GetErrorText(err));
-        goto return_ok;
-
-    return_err:
         return 1;
-
-    return_ok:
+    result_ok:
+        Pa_Terminate();
+        fprintf(stderr, "The device seems to have %u input channels %s\n", num_channel, Pa_GetErrorText(err));
         return 0;
 }
 
 
-
-
-static int _record_callback(
+static int _est_record_callback(
                 const void *inbuf, 
                 const void *outbuf,
                 unsigned long frmsPerBuf, 
@@ -143,11 +149,10 @@ static int _record_callback(
     (void) timeInfo;
     (void) user;
 
-    testdata_t* data = (testdata_t*) user;
-
+    estdata_t* data = (estdata_t*) user;
 
     if (inbuf == NULL)
-        fprintf(stderr, "callback input is NULL\n");
+        return paComplete;
     else {
         size_t chlen = frmsPerBuf / data->num_channels;
         float* interlv = (float*) inbuf;
@@ -168,9 +173,45 @@ static int _record_callback(
         }
     }
     return paComplete;
-    /*data->itcount ++; */
-    /*if (data->itcount > 3)*/
-        /*return paComplete;*/
-    /*else*/
-        /*return paContinue;*/
 }
+
+
+/*static int _record_callback(*/
+                /*const void *inbuf, */
+                /*const void *outbuf,*/
+                /*unsigned long frmsPerBuf, */
+                /*const PaStreamCallbackTimeInfo* timeInfo,*/
+                /*PaStreamCallbackFlags statFlags,*/
+                /*void *user)*/
+/*{*/
+    /*(void) outbuf;*/
+    /*(void) statFlags;*/
+    /*(void) timeInfo;*/
+    /*(void) user;*/
+
+    /*testdata_t* data = (testdata_t*) user;*/
+
+
+    /*if (inbuf == NULL)*/
+        /*fprintf(stderr, "callback input is NULL\n");*/
+    /*else {*/
+        /*size_t chlen = frmsPerBuf / data->num_channels;*/
+        /*float* interlv = (float*) inbuf;*/
+        /*for (unsigned int j=0; j<data->num_channels; j++) {*/
+            /*fprintf(stderr,"c%u: ", j); */
+            /*bool all_zero = true;*/
+            /*float* chp = ((float**) interlv)[j];*/
+            /*for (size_t k=0; k<chlen; k++) {*/
+                /*all_zero = chp[k]==0?true:false;*/
+                /*fprintf(stderr, "%f(%s)", chp[k], (chp[k]==0?"y":"n"));*/
+                /*if (!all_zero)*/
+                    /*break;*/
+            /*} */
+            /*fprintf(stderr,"\n"); */
+            /*if (all_zero) {*/
+                /*data->chlimit_reached = true;*/
+            /*}*/
+        /*}*/
+    /*}*/
+    /*return paComplete;*/
+/*}*/
