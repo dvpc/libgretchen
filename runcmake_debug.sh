a04e1288493432b4308c76f26c18f24bb7a597b0
Tomek <tomekmagard@gmx.net>
2018-12-07 16:08:22 +0100

    Added switch for darwin

--- runcmake_debug.sh
+++ runcmake_debug.sh
@@ -2,10 +2,10 @@
 
 # FIXME
 # this makes osx unuseable. 
-#
-#if [ $BASH_SOURCE=="" ]; then
-#	BASH_SOURCE="$0"
-#fi
+
+if [[ "$OSTYPE" != darwin* && "$BASH_SOURCE" == "" ]]; then
+	BASH_SOURCE="$0"
+fi
 
 bpath="`dirname $BASH_SOURCE`/build"
 if [ ! -d "$bpath" ]; then
